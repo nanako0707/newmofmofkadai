d09a6dc86ad9abfbc3c53315f209ce47ee755527
nanako kan <n.k.0213.0529@gmail.com>
2020-04-30 12:25:10 +0900

    add: 物件一覧画面の実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,15 +1,24 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
+  end
+
+  def show
   end
 
   def new
-    #インスタンスをインスタンス変数に代入。
+    #インスタンスをインスタンス変数に代入。ビューにデータを渡す
     @property = Property.new
   end
 
   def create
-    #paramsメソッドを使用し、送られてきたparameterを全て取得。
-    Property.create(property_name: params[:property][:property_name], rent: params[:property][:rent], street_address: params[:property][:street_address], age: params[:property][:age],
-    note: params[:property][:note])
+    #paramsメソッドを使用し、送られてきたparameterを全て取得。モデルにDB操作の命令を出す。
+    Property.create(property_params)
+    #new_property_pathというPrefixを書くことで、"/properties/new"というURLの指定をします、という意味になる
+    redirect_to new_property_path
+  end
+  private
+  def property_params
+    (params.require(:property).permit(:property_name, :rent, :street_address, :age, :note))
   end
 end
