511ee9e4ba03a295f751bd4c860169a3430e562d
nanako kan <n.k.0213.0529@gmail.com>
2020-05-01 17:34:25 +0900

    add: 賃貸物件と最寄駅のテーブルを関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,14 @@
-ActiveRecord::Schema.define(version: 2020_04_29_073442) do
+ActiveRecord::Schema.define(version: 2020_04_30_234923) do
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string "route"
+    t.string "station"
+    t.integer "minutes_walk"
+    t.integer "property_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_nearest_stations_on_property_id"
+  end
+
   create_table "properties", force: :cascade do |t|
     t.string "property_name"
     t.integer "rent"
