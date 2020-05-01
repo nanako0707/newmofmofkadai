03cae6694b45f91155815d18e96dc435c7ba268c
nanako kan <n.k.0213.0529@gmail.com>
2020-05-01 17:34:25 +0900

    add: 賃貸物件と最寄駅のテーブルを関連付け

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,5 @@ class Property < ApplicationRecord
   validates :rent, presence: true
   validates :street_address, presence: true
   validates :age, presence: true
+  has_many :nearest_stations
 end
